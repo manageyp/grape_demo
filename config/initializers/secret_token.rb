e15cf342b3b9338c7e8e361bcc3b716761c5f6d5
Henry Lee <manageyp@gmail.com>
2013-05-29 21:11:21 +0800

    [NewFeature] init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrapeDemo::Application.config.secret_token = '0261a29f1aee22f44cd8ecc3760649a8e1a4ea8bfba082a58d579ae4480e9fe4267d4c3e0e8311e659f92e907980f7e7f0d87c89c3d63503ac727890fc6c0955'
